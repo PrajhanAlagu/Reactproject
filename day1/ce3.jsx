899c37b8210f84f166bb04e16b39aecc52f58109
PrajhanAlagu <150768539+PrajhanAlagu@users.noreply.github.com>
2024-08-11 20:40:21 +0530

    Update ce3.jsx

--- day1/ce3.jsx
+++ day1/ce3.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const World = () =>
+{
+     return (
+        <div>
+             <p>This is World functional component</p>
+        </div>
+     );
+}
+export default World;
